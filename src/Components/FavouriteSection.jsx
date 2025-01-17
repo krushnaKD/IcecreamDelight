cb3d70d3d163c16777bef9d99aaed38270ec07da
krushana <krushan>
2025-01-17 22:32:50 +0530

    .

---
+++ src/Components/FavouriteSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FavouriteSection = () => {
+  return (
+    <div 
+    //  style="background: linear-gradient(to bottom right, rgba(239,215,239,1), rgba(245,249,252,1), rgba(248,234,225,1), rgba(234,248,249,1));"
+    className='w-full h-screen bg-gradient-to-br from-[rgba(239,215,239,1)] via-[rgba(245,249,252,1)] via-[rgba(248,234,225,1)] to-[rgba(234,248,249,1)]'>
+      
+    </div>
+  )
+}
+
+export default FavouriteSection
